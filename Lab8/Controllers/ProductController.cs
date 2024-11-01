424f9d1fa4b6b4552b74f5ae2daa361b8133788b
Knoflik522 <anncheban31@gmail.com>
2024-11-01 08:25:47 +0200

    Add project files.

---
+++ Lab8/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Lab8.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Lab8.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+            {
+                new Product { ID = 1, Name = "Laptop", Price = 999.99m, CreatedDate = DateTime.Now.AddDays(-10) },
+                new Product { ID = 2, Name = "Phone", Price = 499.99m, CreatedDate = DateTime.Now.AddDays(-5) },
+                new Product { ID = 3, Name = "Tablet", Price = 299.99m, CreatedDate = DateTime.Now }
+            };
+
+            return View(products);
+        }
+    }
+}
